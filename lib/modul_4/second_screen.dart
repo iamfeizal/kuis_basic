5d99f91df2ad0768b0bcf2ed2f9f23135f36f4d0
iamfeizal <imamagusfaisal120@gmail.com>
2022-09-29 23:49:05 +0700

    no modif

--- lib/modul_4/second_screen.dart
+++ lib/modul_4/second_screen.dart
@@ -1,31 +1,32 @@
 import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SecondScreen extends StatelessWidget {
   final String Username;
   final String Password;
-  const SecondScreen({
-    Key? key,
-    required this.Username, // parameter wajib
-    this.Password = "flutter123" // parameter opsional dengan nilai default flutter123
-  }) : super(key: key);
+  const SecondScreen(
+      {Key? key,
+      required this.Username, // parameter wajib
+      this.Password =
+          "flutter123" // parameter opsional dengan nilai default flutter123
+      })
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("second screen"),
-        automaticallyImplyLeading: false, //berfungsi untuk menghilangkan tombol back di kiri atas
-      ),
-      body: ElevatedButton(
-        child: Text("INI LINK"),
-        onPressed: (){
-          _launchURL("https://instagram.com");
-        },
-      )
-    );
+        appBar: AppBar(
+          title: Text("second screen"),
+          automaticallyImplyLeading:
+              false, //berfungsi untuk menghilangkan tombol back di kiri atas
+        ),
+        body: ElevatedButton(
+          child: Text("INI LINK"),
+          onPressed: () {
+            _launchURL("https://instagram.com");
+          },
+        ));
   }
 
   void _launchURL(_url) async {
